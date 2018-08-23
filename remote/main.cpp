21641d9c58ca5474c6d1d7b9c7da77a852354d5c
Aaron <aaron@pnwcab.com>
2018-08-22 22:43:59 -0700

    improved power efficiency (1.3mA @ 3.3V supply)

--- remote/main.cpp
+++ remote/main.cpp
@@ -1,5 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
 
 #include "../pin.h"
 #include "../nrf.h"
@@ -44,13 +46,20 @@ void change_volume(int8_t increment, uint8_t edge_id) {
     }
 }
 
+int knob_timer = 0;
+ISR(PCINT2_vect) {
+    knob_timer = 3000;
+}
+
+
 const uint8_t remote_address[6] = "2Node"; // remote address
 const uint8_t station_address[6] = "1Node"; // receiver address
 int main() {
+    pullup_unused_pins();
     //NRF(Pin irq, Pin ce, Pin cs): irq(irq), ce(ce), cs(cs) {
-    Pin nrf_irq(PORTD, 2),
-        nrf_ce (PORTB, 0),
-        nrf_cs (PORTB, 2);
+    Pin nrf_irq(PORTD, 2, INPUT),
+        nrf_ce (PORTB, 0, OUTPUT),
+        nrf_cs (PORTB, 2, OUTPUT);
     Pin enc_a(PORTD, 6),
         enc_b(PORTD, 7);
     Encoder encoder;
@@ -59,6 +68,7 @@ int main() {
     global_nrf = &nrf;
 
     nrf.init();
+    nrf.set_freq(76);
     nrf.set_tx_addr(station_address);
     nrf.setup_rx_pipe(1, remote_address, 1);
     nrf.start_listening();
@@ -70,26 +80,36 @@ int main() {
     global_nrf->start_listening();
     led = 0;
     _delay_ms(10);
-    //nrf.set_freq(2412);
-    //nrf.config_retransmission(10, 3); // 10 retransmits, 1ms delay
 
-    // uint8_t packet[32];
-    // for(int i=0; i<32; i++)
-    //     packet[i] = 'A' + i;
-    // packet[0] = '?';
+    // enable pin change interrupt on encoder pins
+    PCICR |= _BV(PCIE2);
+    PCIFR |= _BV(PCIE2);
+    PCMSK2 |= _BV(PCINT22) | _BV(PCINT23);
+
     encoder.bind_callback(change_volume);
+
+    sei();
+
     while(1) {
         for(int i=0; i<8; i++) {
-            encoder.update(enc_a << 1 | enc_b);
-            //nrf.stop_listening();
-            //led = 1;
-            //packet[0] = i < 4 ? '?' : '?';
-            //nrf.send(packet, 1); 
-            //nrf.start_listening();
-            //_delay_ms(50);
-            //led = 0;
-            //_delay_ms(950);
-            _delay_ms(1);
+            nrf.power_down();
+            nrf_cs = 1;
+            uint8_t temp = PORTB;
+            PORTB &= 0xC7;
+            set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+            sleep_enable();
+            sleep_bod_disable();
+            //sei();
+            sleep_cpu();
+            sleep_disable();
+            //cli();
+            PORTB = temp;
+            nrf.power_up();
+            while(knob_timer) {
+                encoder.update(enc_a << 1 | enc_b);
+                _delay_us(10);
+                knob_timer--;
+            }
         }
     }
 }
